53df267200ba030dd5f39a2b6903c14a58241a86
Dan McCracken <mccrackend@gmail.com>
2014-07-20 16:57:51 -0700

    WORKING twitter version for real

---
+++ app/src/main/java/com/sagetech/oscon/User.java
@@ -0,0 +1,10 @@
+package com.sagetech.oscon;
+
+import com.google.gson.annotations.SerializedName;
+
+/**
+ * Created by Dan on 7/20/14.
+ */
+public class User {
+    @SerializedName("profile_image_url") public String profileImageUrl;
+}
